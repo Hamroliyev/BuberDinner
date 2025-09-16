5190eb601f5f90774a808478b9e0770cc3d8f707
Kroos <hamroliyevahmadjon1909@gmail.com>
2025-09-16 21:19:25 +0500

    CODE RUB

--- BuberDinner.Api/obj/Debug/net9.0/BuberDinner.Api.AssemblyInfo.cs
+++ BuberDinner.Api/obj/Debug/net9.0/BuberDinner.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d681011331cbbbe423cf41d49f4213f2204ed2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4670bc9018ea6901d65f835c05f82c9350b5eae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
